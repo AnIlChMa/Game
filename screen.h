c429d8705e93b9a4b03278e78538e39fbc443a5c
NastassiaF <32538173+NastassiaF@users.noreply.github.com>
2017-10-16 20:47:05 +0200

    Update screen.h

--- screen.h
+++ screen.h
@@ -8,13 +8,14 @@ class screen
         screen();
         virtual ~screen();
         inizializzafinestra();
+        stampadentromappa(char* test); //per stampare dentro mappa
         WINDOW *win1, *win2;
     protected:
         int x, y;
         int altezza, larghezza;
         //sottofinestre per il gioco
 
-        //ci dice se è o meno una sottofinestra
+        //ci dice se Ã¨ o meno una sottofinestra
         bool flag;
         //puntatore finestra padre(terminale)
         WINDOW *padre;
