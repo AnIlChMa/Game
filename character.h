055e82247f55e7c2f96824530f88bc149c8579e3
IlariaRinaldi <30722713+IlariaRinaldi@users.noreply.github.com>
2017-10-25 09:49:01 +0200

    Add files via upload

---
+++ character.h
@@ -0,0 +1,19 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+#include <string.h>
+
+
+class character
+{
+    public:
+        character();
+        virtual ~character();
+
+    protected:
+
+        bool alive;
+
+    private:
+};
+
+#endif // CHARACTER_H
